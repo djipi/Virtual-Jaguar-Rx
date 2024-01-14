ba031e93b8f8591ec6a65e8f1c55155dd924b207
Jean-Paul Mari <djipi.mari@gmail.com>
2024-01-14 06:21:27 -0500

    Added support for SDL2 library
    SDL2 2.28.5 library 64bits for VS 2017.

--- src/gui/gamepad.cpp
+++ src/gui/gamepad.cpp
@@ -5,12 +5,15 @@
 // (C) 2013 Underground Software
 //
 // JLH = James Hammons <jlhamm@acm.org>
+// JPM = Jean-Paul Mari <djipi.mari@gmail.com>
 //
 // Who  When        What
 // ---  ----------  ------------------------------------------------------------
 // JLH  01/05/2013  Created this file
+// JPM   Jan./2024  Support SDL2 library
 //
 
+#include <string.h>
 #include "gamepad.h"
 #include "log.h"
 
@@ -55,7 +58,11 @@ void Gamepad::AllocateJoysticks(void)
 		// We need to copy the contents of this pointer, as SDL will change it
 		// willy nilly to suit itself
 //		padName[i] = SDL_JoystickName(i);
+#if SDL_MAJOR_VERSION < 2
 		strncpy(padName[i], SDL_JoystickName(i), 127);
+#else
+		strncpy(padName[i], SDL_JoystickName(pad[i]), 127);
+#endif
 		padName[i][127] = 0;	// Just in case name's length > 127
 
 		if (pad[i])
